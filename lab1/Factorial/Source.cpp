1669c5ba3dade38d950daabc05d50bf568269f1d
Alexander Antonov <saha_97@bk.ru>
2017-11-27 11:56:53 +0300

    Add comments

--- lab1/Factorial/Source.cpp
+++ lab1/Factorial/Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
+//Our functioun
 unsigned int factorial(unsigned int number) {
 	if (number == 0) return 1;
 	return number * factorial(number - 1);
 }
 
 int main() {
-	unsigned int number;
-	std::cin >> number;
-	std::cout << factorial(number) << std::endl;
+	unsigned int number; 
+	std::cin >> number; // Enter the number
+	std::cout << factorial(number) << std::endl; // Get result of our function
 }
